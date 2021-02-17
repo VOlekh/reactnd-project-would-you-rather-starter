a317d27aa09919cd175652a5a6dba3113a3e4e43
Valentina-Olekhnovich <volekh@gmail.com>
2021-02-17 22:37:51 +0100

    Update UI

--- src/components/Question.js
+++ src/components/Question.js
@@ -1,13 +1,27 @@
 import React, { Component } from 'react'
 import { connect } from 'react-redux'
-import { formatQuestion } from '../utils/helper'
+import { formatDate, formatQuestion } from '../utils/helper'
 
 class Question extends Component{
     render(){
         console.log(this.props)
+        const{question} = this.props
+        const {
+            name, avatar, timestamp, optionOne, optionTwo } = question
         return(
             <div className='question'>
-                {}
+                <img 
+                src = {avatar}
+                alt = {`Avatar of ${name}`}
+                className='avatar'
+                />
+                <div className='question-info'>
+                    <span>{name}</span>
+                    <span> {formatDate(timestamp)}</span>
+                    <div>{optionOne.text}</div>
+                    <div>{optionTwo.text}</div>
+
+                </div>
             </div>
         )
     }
