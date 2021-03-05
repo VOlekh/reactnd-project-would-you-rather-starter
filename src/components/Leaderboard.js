80fa15c5b10b743173ebff86b519a672500d85e3
Valentina-Olekhnovich <volekh@gmail.com>
2021-03-05 20:16:58 +0100

    Add score calculation

--- src/components/Leaderboard.js
+++ src/components/Leaderboard.js
@@ -1,14 +1,80 @@
-import React, {Component} from 'react';
-import {connect} from 'react-redux';
+import React, { Component } from "react";
+import { formatDate, formatQuestion } from '../utils/helper'
+import Question from "./Question";
+import { connect } from "react-redux";
 
 class Leaderboard extends Component {
-    render() {
+  render() {
+    const { users } = this.props
+    console.log("Dictionary/ leaderbord:", users)
+    console.log("Array/ leaderbord:", Object.values(users))
+    //The length property of an object which is an instance of type Array sets or returns the number of elements in that array. 
+   
+    // console.log("scoreQustions:", scoreQustions )
+    return (
+        <div>
+            <ul className="leaderbord">
+                {Object.values(users).map((user) => 
+                        (
+                        <div className='user_leaderbord_card' key={user.id}> 
+                        {user.name}
+                        <img src = {user.avatarURL} alt = {`Avatar of ${user.name}`} className='avatar' />                    
+                         {/* Array - massive */}
+                        <div> Score questions asked: {user.questions.length}</div>
+                        {/* Dictionary, map, slovar */}
+                        <div>Score questions answered:{Object.keys(user.answers).length}</div>
+                                       
+                        </div>
+                        ))
+                        }
+        
+{/* 
+                     <div className='question-info'>
+                        <h3>Leaderboard results</h3>
+                            <div className="question-text-preview">
+                                    Score questions asked: {Object.values(users).map((user) =>
+                                    user.questions.length)}
+                            </div>
+                            <div>
+                                    Score questions answered: {Object.values(users).map((user) =>
+                                    user.answers.length)}
+                             </div>
+                                 <div>
+                                    Total: {scoreQustions + scoreAnswers}
+                                 </div>
+                            </div> */}
+                   
+                
+            </ul>
+        </div>
+     );
+   }
+}
 
-        return (
-            <div className='center'>
-               Leaderboard
-            </div>
-        )
+// function mapStateToProps({ users}) {
+//   // let usersTotalScore =
+//   let questionsScore = Object.values(users).map((user) => ({
+//     scoreQustions: {user.questions}.length,
+//   }));
+//   console.log("mapStateProps/ leaderboard:", users)
+
+//   let answersScore = Object.values(users).map((user) => ({
+//     scoreAnswers: Object.keys(user.answeres).length,
+//   }));
+
+//   return {
+//     usersQuestionsScoreIDs:
+//     questionsScore
+//     .sort((a, b) => b.score - a.score)
+//     .map(({id})=>id),
+//   };
+// }
+function mapStateToProps({users}) {
+    console.log("mapStateProps/ leaderbord:", users)
+    return{
+        users    
     }
 }
-export default (Leaderboard);
+
+
+export default connect(mapStateToProps)(Leaderboard);
