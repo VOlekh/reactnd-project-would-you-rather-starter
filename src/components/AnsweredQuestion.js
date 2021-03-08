b9e26701396bfa3cc1e66b282e03d31f6840c9cf
Valentina-Olekhnovich <volekh@gmail.com>
2021-03-08 23:47:32 +0100

    add dummy component

---
+++ src/components/AnsweredQuestion.js
@@ -0,0 +1,78 @@
+import React, { Component } from 'react'
+import { connect } from 'react-redux'
+import { formatDate, formatQuestion } from '../utils/helper'
+import {handleQuestionAnswer} from '../actions/questions'
+import Button from 'react-bootstrap/Button';
+
+class AnsweredQuestion extends Component{
+
+    onAnswerChange = (answer) => {
+        console.log(answer);
+        this.setState(() => ({
+            answer
+          }))
+    }
+
+    onAnswerSubmit = (e) => {
+        e.preventDefault();
+        const { dispatch, question, authedUser } = this.props;
+        const {answer} = this.state;
+
+    
+        dispatch(handleQuestionAnswer({
+            qid: question.id,
+            answer,
+            authedUser
+            }));
+      }
+
+    render(){
+        // console.log(this.props)
+        const{question, authedUser} = this.props
+        console.log(authedUser)
+        const {name, avatar, timestamp, optionOne, optionTwo } = question
+
+        return(
+            
+            <div className='question'>
+                <img 
+                src = {avatar}
+                alt = {`Avatar of ${name}`}
+                className='avatar'
+                />
+                
+                <div className='question-info'>
+                    
+                    <span>{name}</span>
+                    <span> {formatDate(timestamp)}</span>
+                
+                    <h3>Would you rather ...</h3>
+                    <RadioGroup onChange={ this.onAnswerChange } >
+                        <RadioButton value="optionOne" >
+                          {optionOne.text}
+                        </RadioButton>
+                        <RadioButton value="optionTwo" >
+                          {optionTwo.text}
+                        </RadioButton>
+                    </RadioGroup>
+                <Button variant="primary" onClick={this.onAnswerDetails} type="submit">
+                    Details
+                </Button>
+                </div>
+            </div>
+        )
+    }
+}
+
+
+function mapStateToProps({authedUser, users, questions}, {id}) {
+    const question = questions[id]
+    console.log(authedUser)
+    return{
+        authedUser,
+        question: formatQuestion(question, users[question.author], authedUser)
+      
+    }
+}
+
+export default connect(mapStateToProps) (AnsweredQuestion)
--- src/components/Logout.js
+++ src/components/Logout.js
@@ -1,14 +1,32 @@
 import React, {Component} from 'react';
 import {connect} from 'react-redux';
+import Card from 'react-bootstrap/Card';
 
 class Logout extends Component {
+    state = {
+        redirect: false
+    }
+
     render() {
 
         return (
-            <div className='center'>
-               Logout
-            </div>
+            <Card>
+                <Card.Body>
+                <div className="center">
+                    <h3>You are logged out</h3>
+                   
+                </div>
+                </Card.Body>
+            </Card>    
+         
         )
     }
 }
-export default (Logout);
+
+function mapStateToProps({ authedUser }) {
+    return {
+        authedUser
+    }
+}
+
+export default connect(mapStateToProps)(Logout);
