295ab0c3505f0d96010d257714b3cfee7e3b83f6
Valentina-Olekhnovich <volekh@gmail.com>
2021-03-08 23:45:01 +0100

    Chenge ViewPoll

--- src/components/Question.js
+++ src/components/Question.js
@@ -1,87 +1,101 @@
-import React, { Component } from 'react'
-import { connect } from 'react-redux'
-import { formatDate, formatQuestion } from '../utils/helper'
-import {RadioGroup, RadioButton} from 'react-radio-buttons'
-import {handleQuestionAnswer} from '../actions/questions'
-
-class Question extends Component{
-
-    onAnswerChange = (answer) => {
-        console.log(answer);
-        this.setState(() => ({
-            answer
-          }))
-    }
-
-    onAnswerSubmit = (e) => {
-        // e.preventDefault();
-    
-        // todo: Handle Like Tweet
-        const { dispatch, question, authedUser } = this.props;
-        const {answer} = this.state;
-
-    
-        dispatch(handleQuestionAnswer({
-            qid: question.id,
-            answer,
-            authedUser
-            }));
-      }
-
-    render(){
-        // console.log(this.props)
-        const{question, authedUser} = this.props
-        console.log(authedUser)
-        const {
-            name, avatar, timestamp, optionOne, optionTwo } = question
-        return(
-            
-            <div className='question'>
-                <img 
-                src = {avatar}
-                alt = {`Avatar of ${name}`}
-                className='avatar'
-                />
-                
-                <div className='question-info'>
-                    
-                    <span>{name}</span>
-                    <span> {formatDate(timestamp)}</span>
-                
-                    <h3>Would you rather ...</h3>
-                    <RadioGroup onChange={ this.onAnswerChange } >
-                        <RadioButton value="optionOne" >
-                          {optionOne.text}
-                        </RadioButton>
-                        <RadioButton value="optionTwo" >
-                          {optionTwo.text}
-                        </RadioButton>
-                    </RadioGroup>
-
-                <button
-                    className='btn'
-                    onClick={this.onAnswerSubmit}>
-                     {/* //TBD: disabled if the radio button is not checked*/}
-                     {/* 'value' is not defined  no-undef
-                    disabled={ value === false} */}
-                    Submit
-                </button>
-
-                </div>
-            </div>
-        )
-    }
-}
+import React, { Component } from "react";
+import { connect } from "react-redux";
+import { formatDate, formatQuestion } from "../utils/helper";
+import { useHistory } from 'react-router-dom'
+import { handleQuestionAnswer } from "../actions/questions";
+import Button from "react-bootstrap/Button";
+import Card from "react-bootstrap/Card";
+import Media from "react-bootstrap/Media";
+import Truncate from 'react-truncate';
 
 
-function mapStateToProps({authedUser, users, questions}, {id}) {
-    const question = questions[id]
-    console.log(authedUser)
-    return{
+ class Question extends Component {
+  onAnswerChange = (answer) => {
+    console.log(answer);
+    this.setState(() => ({
+      answer,
+    }));
+  };
+
+  onAnswerSubmit = (e) => {
+    e.preventDefault();
+    const { dispatch, question, authedUser } = this.props;
+    const { answer } = this.state;
+
+    dispatch(
+      handleQuestionAnswer({
+        qid: question.id,
+        answer,
         authedUser,
-        question: formatQuestion(question, users[question.author], authedUser)
-      
-    }
+      })
+    );
+  };
+
+
+
+    // const history = useHistory()
+
+    // function onViewPoll() {
+    //     history.push(`/questions/${question.id}`)
+    // }
+
+  render() {
+    // console.log(this.props)
+    const { question, authedUser } = this.props;
+    console.log(authedUser);
+    const { name, avatar, timestamp, optionOne, optionTwo } = question;
+    const TextTruncate = require('react-text-truncate'); 
+
+    return (
+      <Card>
+        <Card.Body>
+          <div>
+            <Media>
+            <img
+                width={64}
+                height={64}
+                className="mr-3"
+                src={avatar}
+                alt={`Avatar of ${name}`} 
+            />
+            <Media.Body>
+                <h5>{name}</h5>
+                <p>{formatDate(timestamp)}</p>
+            </Media.Body>
+            </Media>
+            <h4>Would you rather ...</h4>
+              <div>
+                <Truncate lines={1} ellipsis={<span>... </span>}>
+                    ...{optionOne.text}...
+                </Truncate>
+              </div>
+
+              <div>
+                <Truncate lines={1} ellipsis={<span>... </span>}>
+                    ...{optionTwo.text}...
+                </Truncate>
+              </div>
+            </div>
+        </Card.Body>
+        <Button
+            variant="primary"
+            onClick={this.onViewPoll}
+            type="viewPoll"
+            >
+            View Poll
+        </Button>
+      </Card>
+    );
+  }
 }
 
-export default connect(mapStateToProps) (Question)
+function mapStateToProps({ authedUser, users, questions }, { id }) {
+  const question = questions[id];
+  console.log(authedUser);
+  return {
+    authedUser,
+    question: formatQuestion(question, users[question.author], authedUser),
+  };
+}
+
+export default connect(mapStateToProps)(Question);
